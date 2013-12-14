ba3730e21a33009120b40295e40e4bec625d6f3a
Matt Esch <matt@mattesch.info>
2013-12-14 23:10:00 +0000

    cleaner interface

--- calc.h
+++ calc.h
@@ -1,18 +1,13 @@
-typedef double (*binary)(const double, const double);
-typedef double (*unary)(const double);
-typedef double (*apply_binary);
-typedef double (*apply_unary)(unary, double, double);
-
 typedef struct {
     double (*applyb)(binary, const double, const double);
     double (*applyu)(unary, const double);
-    binary add;
-    binary subtract;
-    binary multiply;
-    binary divide;
-    binary power;
-    unary increment;
-    unary decrement;
+    double (*add)(const double, const double);
+    double (*subtract)(const double, const double);
+    double (*multiply)(const double, const double);
+    double (*divide)(const double, const double);
+    double (*power)(const double, const double);
+    double (*increment)(const double);
+    double (*decrement)(const double);
 } calc_interface;
 
 const calc_interface calc_exports;
